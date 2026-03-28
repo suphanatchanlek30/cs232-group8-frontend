72962cdee5518dd28a9f16c06b8d6397dee6f5d2
chisapat <chisapat25@gmail.com>
2026-03-28 20:35:31 +0700

    add search bar

--- app/(public)/track-status/page.tsx
+++ app/(public)/track-status/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+import SearchBar from "@/components/track-status/track-search";
+
 export default function TrackPage() {
   return (
     <div className="min-h-screen bg-white">
       <div className="mx-auto w-full max-w-3xl">
         <h1>Track Status</h1>
+        <SearchBar onSearch={(id) => console.log(id)} />
       </div>
     </div>
   );
